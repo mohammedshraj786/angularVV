a95feba00e161bf80f921a9fffca025cf484d1da
ND <mohammed@navadhiti.com>
2023-11-21 17:24:22 +0530

    template driven from with extra feilds and validation

--- template-driven-form/AngularForms/src/app/template-driven-form/template-driven-form.component.ts
+++ template-driven-form/AngularForms/src/app/template-driven-form/template-driven-form.component.ts
@@ -1,27 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-template-driven-form',
   templateUrl: './template-driven-form.component.html',
   styleUrls: ['./template-driven-form.component.css']
 })
-export class TemplateDrivenFormComponent implements OnInit {
+export class TemplateDrivenFormComponent  {
+  age: string | undefined;
+  selectedImage: File | null = null;
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-  onSubmit(form:any)
-  {
-    if(form.valid)
-    {
-      console.log(form,"Success");
-      
-    }
-    else{
+  onSubmit(myData: any) {
+    if (myData.valid) {
+      console.log(myData, "Success");
+    } else {
       console.log("Failed");
-      
     }
   }
+
+  calculateAge(dob: string): void {
+    if (dob) {
+      const today = new Date();
+      const birthDate = new Date(dob);
+      let age = today.getFullYear() - birthDate.getFullYear();
+      const monthDiff = today.getMonth() - birthDate.getMonth();
+      if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+      }
+      this.age = age.toString(); 
+    }
+  }
+  
 }
