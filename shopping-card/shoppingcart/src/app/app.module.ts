36cc5e44f052f6c76bde66645e033276741402d6
mohammedshraj786 <mohammed@navadhiti.com>
2023-11-30 11:51:50 +0530

    Creating components

--- shopping-card/shoppingcart/src/app/app.module.ts
+++ shopping-card/shoppingcart/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ComponentsRoutingModule } from './components/components-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ComponentsRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ shopping-card/shoppingcart/src/app/components/components-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from "@angular/core";
+import { RouterModule , Routes } from "@angular/router";
+
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
+import { SidebarComponent } from "./sidebar/sidebar.component";
+
+const routes:Routes = 
+[
+    {path:'header',component:HeaderComponent},
+    {path:'footer',component:FooterComponent},
+    {path:'sidebar',component:SidebarComponent}
+];
+
+@NgModule({
+
+    imports:[RouterModule.forChild(routes)],
+    exports:[RouterModule]
+})
+export class ComponentsRoutingModule {}
